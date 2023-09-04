442ba9c0b3cc4a4ad2a5ea935e897c2825003532
nohimgodage <nohim.godage@gmail.com>
2023-09-04 22:42:37 +0530

    Starting Bad Desing

--- src/DemoPage/DemoPageComponent.tsx
+++ src/DemoPage/DemoPageComponent.tsx
@@ -1,7 +1,42 @@
+import Comments from "../SharableModule/BadDesign/Comments";
+
+
 const DemoPageComponent = () => {
+
+    const badDeSignComponents = () => {
+        return (
+            <>
+                <div>
+                    <h4> Client Requirement 1</h4>
+                    <Comments
+                        requirementId={1}
+                        postOrCommentId={1}
+                    />
+                </div>
+
+                <div>
+                    <h4> Client Requirement 2</h4>
+                    <Comments
+                        requirementId={2}
+                        postOrCommentId={1}
+                    />
+                </div>
+
+                <div>
+                    <h4> Client Requirement 3</h4>
+                    <Comments
+                        requirementId={3}
+                        postOrCommentId={1}
+                    />
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
-            Demo Page
+            <h2>Demo Page</h2>
+            {badDeSignComponents()}
         </>
     );
 }
---
+++ src/SharableModule/BadDesign/Comments.tsx
@@ -0,0 +1,14 @@
+
+interface IComments {
+    requirementId: number;
+    postOrCommentId: number
+}
+
+const Comments = (props: IComments) => {
+    return (
+        <>
+            <h6>Comments</h6>
+        </>
+    );
+}
+export default Comments;
