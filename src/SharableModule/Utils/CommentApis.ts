ce1f0ae2ec6e2755de927c0a04e8b1bf0bac9b80
nohimgodage <nohim.godage@gmail.com>
2023-09-04 23:33:08 +0530

    Fix API

--- src/SharableModule/Utils/CommentApis.ts
+++ src/SharableModule/Utils/CommentApis.ts
@@ -2,11 +2,11 @@ import get from "./CoreApis";
 
 const CommentApis = {
     getCommentById: async (id: number) => {
-        const result = await get(`/comments/${id}`);
+        const result = await get(`comments/${id}`);
         return result;
     },
     getCommentsByPostId: async (postId: number) => {
-        const result = await get(`/comments?postId=${postId}`);
+        const result = await get(`comments?postId=${postId}`);
         return result;
     }
 };
