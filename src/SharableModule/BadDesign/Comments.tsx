0c353dd65c468ab92e73dc872a2eae6c8eb509b8
nohimgodage <nohim.godage@gmail.com>
2023-09-05 23:30:46 +0530

    Updating Comments.tsx with MUI

--- src/SharableModule/BadDesign/Comments.tsx
+++ src/SharableModule/BadDesign/Comments.tsx
@@ -17,26 +17,46 @@ const Comments = (props: IComments) => {
 
     const [commentsList, setCommentsList]
         = useState<IComment[]>(() => []);
+
+    const {requirementId, contextType, contextId} = props;
+
+    const fontColor
+        = requirementId === 1
+        ? 'red'
+        : requirementId === 2
+            ? 'blue'
+            : 'green';
+
     const getListItem = (item: IComment) => {
+
+        const nameOrEmail = requirementId === 1
+            ? `Name: ${item.name}`
+            : requirementId === 2
+                ?`Email: ${item.email}`
+                : null;
+
         return (
             <div key={item.id}>
-                <ListItem alignItems="flex-start" >
-                    <ListItemText
-                        primary={item.name}
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    sx={{display: 'inline'}}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    {item.email}
-                                </Typography>
-                                {item.body}
-                            </React.Fragment>
-                        }
-                    />
+                <ListItem alignItems="flex-start">
+                    <Typography
+                        color={fontColor}
+                    >
+                        <ListItemText
+                            primary={nameOrEmail}
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        sx={{display: 'inline'}}
+                                        component="span"
+                                        variant="body2"
+                                        color={fontColor}
+                                    >
+                                        Comment: {item.body}
+                                    </Typography>
+                                </React.Fragment>
+                            }
+                        />
+                    </Typography>
                 </ListItem>
                 <Divider variant="inset" component="li"/>
             </div>
@@ -59,15 +79,24 @@ const Comments = (props: IComments) => {
     }
 
     useEffect(() => {
-        CommentApis
-            .getCommentsByPostId(props.contextId)
-            .then(result => {
-                setCommentsList(result);
-            });
-    }, [props.contextId, props.contextType]);
+        if (contextType === 'POST') {
+            CommentApis
+                .getCommentsByPostId(contextId)
+                .then(result => {
+                    setCommentsList(result);
+                });
+        } else {
+            CommentApis
+                .getCommentById(contextId)
+                .then(result => {
+                    setCommentsList([result]);
+                });
+        }
+
+    }, [contextId, contextType]);
 
     return (
-        <div style={{margin: 'auto', width:'40%'}}>
+        <div style={{margin: 'auto', width: '40%'}}>
             <h6>Comments</h6>
             {getCommentsList()}
         </div>
